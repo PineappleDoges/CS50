271bc7f02658a49a3e6db31fcd1978c96baa4a7c
PineappleDoge <keyizhang0624@gmail.com>
2025-01-04 13:38:01 +0100

    Add files via upload

---
+++ Week_2/caesar.c
@@ -0,0 +1,54 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool only_digits(string s);
+char rotate(char c, int n);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2 || !only_digits(argv[1]))
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    int key = atoi(argv[1]);
+
+    string plaintext = get_string("plaintext: ");
+
+    printf("ciphertext: ");
+    for (int i = 0, len = strlen(plaintext); i < len; i++)
+    {
+        printf("%c", rotate(plaintext[i], key));
+    }
+    printf("\n");
+    return 0;
+}
+
+bool only_digits(string s)
+{
+    for (int i = 0, len = strlen(s); i < len; i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(char c, int n)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return 'A' + (c - 'A' + n) % 26;
+    }
+    else if (c >= 'a' && c <= 'z')
+    {
+        return 'a' + (c - 'a' + n) % 26;
+    }
+    return c;
+}
