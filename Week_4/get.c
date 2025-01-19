841b8a95dec982c19b57843598e017186e56d68f
PineappleDoge <keyizhang0624@gmail.com>
2025-01-19 12:51:41 +0800

    Add files via upload

---
+++ Week_4/get.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    // int n;
+    // printf ("n: ");
+    // scanf ("%i", &n);
+    // printf("n: %i\n", n);
+
+    char s[4];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
