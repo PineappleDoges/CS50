cd2ea792cdd9bf9f41e3aa0c8d099d57b91c4c1f
PineappleDoge <keyizhang0624@gmail.com>
2025-01-23 18:19:58 +0800

    Add files via upload

---
+++ Week_6/agree.py
@@ -0,0 +1,13 @@
+s = input ("Do you agree: ").lower()
+
+# if s == "Y" or s == "y":
+#     print("Agreed.")
+# elif s == "N" or s == "n":
+#     print("Not agreed.")
+
+# t = s.lower()
+
+if t in ["y", "yes"]:
+    print("Agreed.")
+elif t in ["n", "no"]:
+    print("Not agreed.")
