bccebd048945dc47c7797888ac3cd847f3b7cc73
PineappleDoge <keyizhang0624@gmail.com>
2025-01-23 19:10:58 +0800

    Add files via upload

---
+++ Week_6/phonebook.py
@@ -0,0 +1,12 @@
+names = ["Yuliia", "David", "John"]
+
+name = input("Name: ")
+
+# for n in names:
+#     if name == n:
+if name in names
+        print("Found")
+        break
+
+else:
+    print("Not Found")
